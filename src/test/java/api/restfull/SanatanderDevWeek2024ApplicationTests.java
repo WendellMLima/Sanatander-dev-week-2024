eb59d91090c1552cab97dcf8b2bf02d800b15594
WendellLima <wendellmenezes58@gmail.com>
2024-08-18 11:53:04 -0300

    Initial commit

---
+++ src/test/java/api/restfull/SanatanderDevWeek2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package api.restfull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SanatanderDevWeek2024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
